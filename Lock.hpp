41a3d96a9676bf09405e12eb8f214b14b36df6e6
RmbRT <steffen.rattay@gmail.com>
2015-05-22 03:24:08 +0200

    :night_with_stars: Tidied up namespace lock. Fixed comments
    moved exceptions and try lock functions into lock::helper

--- Lock.hpp
+++ Lock.hpp
@@ -7,15 +7,6 @@
 
 namespace lock
 {
-
-	struct bad_read_unlock { };
-	struct bad_write_unlock { };
-	struct bad_write { };
-	struct bad_read { };
-	struct bad_move_write_lock { };
-	struct bad_thread_safe_move { };
-	struct bad_thread_safe_destruct { };
-
 	template<class T>
 	class WriteLock;
 	template<class T>
@@ -23,8 +14,17 @@ namespace lock
 	template<class T>
 	class ThreadSafe;
 	
+	/*Helper namespace with helper functions and data types that are only of internal use.*/
 	namespace helper
 	{
+		struct bad_read_unlock { };
+		struct bad_write_unlock { };
+		struct bad_write { };
+		struct bad_read { };
+		struct bad_move_write_lock { };
+		struct bad_thread_safe_move { };
+		struct bad_thread_safe_destruct { };
+
 		template<class T>
 		struct read_lock_pair
 		{
@@ -45,28 +45,60 @@ namespace lock
 		inline bool try_read_lock(void) { return true; }
 		inline bool try_write_lock(void) { return true; }
 		inline bool try_any_lock(void) { return true; }
+
+		template<class T, class ...Targs>
+		bool try_any_lock(T &arg, Targs&... args);
+		template<class T, class ...Targs>
+		inline bool try_any_lock(helper::read_lock_pair<T>& arg, Targs&...args);
+		template<class T, class ...Targs>
+		inline bool try_any_lock(helper::write_lock_pair<T>& arg, Targs&...args);
+
+		template<class T, class ...Targs>
+		bool try_any_lock(T &arg, Targs&... args);
+
+		template<class T, class ...Targs>
+		inline bool try_any_lock(helper::write_lock_pair<T> &arg, Targs&... args)
+		{	using namespace helper;
+			return arg.thread_safe.try_lock_write(arg.lock) && try_any_lock(args...);
+		}
+
+		template<class T, class ...Targs>
+		inline bool try_any_lock(helper::read_lock_pair<T> &arg, Targs&... args)
+		{	using namespace helper;
+			return arg.thread_safe.try_lock_read(arg.lock) && try_any_lock(args...);
+		}
+
+		template<class T, class ...Targs>
+		/*Tries to lock all the passed ThreadSafe with their paired ReadLock. Returns true, if all locks were successful.
+		See ThreadSafe::try_lock_read.*/
+		inline bool try_read_lock(helper::read_lock_pair<T> &arg, helper::read_lock_pair<Targs>&... args)
+		{
+			using namespace helper;
+			return arg.thread_safe.try_lock_read(arg.lock) && try_read_lock(args...);
+		}
+
+		template<class T, class ...Targs>
+		/*Tries to lock all the passed ThreadSafe with their paired WriteLock. Returns true, if all locks were successful.
+		See ThreadSafe::try_lock_write.*/
+		inline bool try_write_lock(helper::write_lock_pair<T> &arg, helper::write_lock_pair<Targs>&... args)
+		{	using namespace helper;
+			return arg.thread_safe.try_lock_write(arg.lock) && try_write_lock(args...);
+		}
 	}
 
 	template<class T>
-	/*Use this function to pass a (ReadLock, ThreadSafe) pair to the locking functions try_read_lock and multi_read_lock.*/
+	/*Use this function to pass a (ReadLock, ThreadSafe) pair to the locking functions lock::multi_lock and lock::multi_read_lock.*/
 	inline helper::read_lock_pair<T> pair(ReadLock<T> &lock, ThreadSafe<T> &thread_safe) { return { lock, thread_safe }; }
 
 	template<class T>
-	/*Use this function to pass a (WriteLock, ThreadSafe) pair to the locking functions try_write_lock and multi_write_lock.*/
+	/*Use this function to pass a (WriteLock, ThreadSafe) pair to the locking functions lock::multi_lock and lock::multi_write_lock.*/
 	inline helper::write_lock_pair<T> pair(WriteLock<T> &lock, ThreadSafe<T> &thread_safe) { return { lock, thread_safe }; }
 
-	template<class T, class ...Targs>
-	/*Tries to lock all the passed ThreadSafe with their paired ReadLock. Returns true, if all locks were successful.
-	See ThreadSafe::try_lock_read.*/
-	inline bool try_read_lock(helper::read_lock_pair<T> &arg, helper::read_lock_pair<Targs>&... args)
-	{
-		using namespace helper;
-		return arg.thread_safe.try_lock_read(arg.lock) && try_read_lock(args...);
-	}
 
 	template<class ...T>
 	/*Locks multiple thread safe objects for reading, and sets the passed read locks to their corresponding thread safe object.
-	This function releases all locks and tries to re-lock all locks in case one or more resources could not be locked, to prevent dead locks.*/
+	This function releases all locks and tries to re-lock all locks in case one or more resources could not be locked, to prevent dead locks.
+	Note: If you have to read lock as well as write lock objects for a function / operation, use lock::multi_lock instead.*/
 	void multi_read_lock(helper::read_lock_pair<T>&... args)
 	{	using namespace helper;
 		// try to lock all thread safe objects.
@@ -74,45 +106,17 @@ namespace lock
 			// not all resources could be locked. Release all locks, try again.
 			destructor(args.lock...);
 	}
-	template<class T, class ...Targs>
-	/*Tries to lock all the passed ThreadSafe with their paired WriteLock. Returns true, if all locks were successful.
-	See ThreadSafe::try_lock_write.*/
-	inline bool try_write_lock(helper::write_lock_pair<T> &arg, helper::write_lock_pair<Targs>&... args)
-	{	using namespace helper;
-		return arg.thread_safe.try_lock_write(arg.lock) && try_write_lock(args...);
-	}
 
 	template<class ...T>
 	/*Locks multiple thread safe objects for writing, and sets the passed write locks to their corresponding thread safe object.
-	This function releases all locks and tries to re-lock all locks in case one or more resources could not be locked, to prevent dead locks.*/
+	This function releases all locks and tries to re-lock all locks in case one or more resources could not be locked, to prevent dead locks.
+	Note: If you have to read lock as well as write lock objects for a function / operation, use lock::multi_lock instead.*/
 	void multi_write_lock(helper::write_lock_pair<T>&... args)
 	{	using namespace helper;
 		// try to lock all thread safe objects.
 		while(!try_write_lock(args...))
 			// not all resources could be locked. Release all locks, try again.
 			destructor(args.lock...);
-	}
-
-	template<class T, class ...Targs>
-	bool try_any_lock(T &arg, Targs&... args);
-	template<class T, class ...Targs>
-	inline bool try_any_lock(helper::read_lock_pair<T>& arg, Targs&...args);
-	template<class T, class ...Targs>
-	inline bool try_any_lock(helper::write_lock_pair<T>& arg, Targs&...args);
-
-	template<class T, class ...Targs>
-	bool try_any_lock(T &arg, Targs&... args) { static_assert("please only pass values created with lock::pair()."); }
-
-	template<class T, class ...Targs>
-	inline bool try_any_lock(helper::write_lock_pair<T> &arg, Targs&... args)
-	{	using namespace helper;
-		return arg.thread_safe.try_lock_write(arg.lock) && try_any_lock(args...);
-	}
-
-	template<class T, class ...Targs>
-	inline bool try_any_lock(helper::read_lock_pair<T> &arg, Targs&... args)
-	{	using namespace helper;
-		return arg.thread_safe.try_lock_read(arg.lock) && try_any_lock(args...);
 	}
 
 	template<class ...T>
@@ -129,7 +133,9 @@ namespace lock
 
 	template<class T>
 	/*Wrapper class for shared resources.
-	Use in combination with ReadLock and WriteLock, as well as the multi_read_lock and multi_write_lock functions to ensure thread safety.
+	Use in combination with ReadLock and WriteLock, as well as the multi_lock function to ensure thread safety and prevent dead locks.
+	To be explicit about only read locking / write locking, use multi_read_lock and multi_write_lock.
+	A function / operation should ony have one lock call to acquire its locks. This prevents dead locks / incomplete locking of needed resources.
 	Note that only one write lock may be attached to every shared resource at a time.
 	A shared resource can be read locked multiple times at once. Only when all read locks are lifted, the shared resource is unlocked.
 	While a shared resource is write locked, it can not be read locked.
@@ -155,17 +161,17 @@ namespace lock
 		template<class ...Args>
 		ThreadSafe(Args&&... args) : _mutex(), _write_lock(nullptr), _read_lock(0), obj(std::forward<Args>(args)...) { }
 
-		ThreadSafe(_MyT && move) throw (bad_thread_safe_move)
+		ThreadSafe(_MyT && move) throw (helper::bad_thread_safe_move)
 			: _mutex(), _write_lock(move._write_lock), _read_lock(move._read_lock), obj(std::move(move.obj))
 		{
 			if(_write_lock || _read_lock)
-				throw bad_thread_safe_move();
+				throw helper::bad_thread_safe_move();
 		}
 
-		~ThreadSafe() throw (bad_thread_safe_destruct)
+		~ThreadSafe() throw (helper::bad_thread_safe_destruct)
 		{
 			if(_write_lock || _read_lock)
-				throw bad_thread_safe_destruct();
+				throw helper::bad_thread_safe_destruct();
 		}
 
 		template<class ...Args>
@@ -218,16 +224,6 @@ namespace lock
 				return false;
 		}
 
-
-		inline const T &read_unsafe()
-		{
-			return obj;
-		}
-		inline T &write_unsafe()
-		{
-			return obj;
-		}
-
 	private:
 		void lock_write(_WriteLock * key)
 		{
@@ -243,21 +239,21 @@ namespace lock
 			}
 		}
 
-		void move_write_lock(_WriteLock * oldKey, _WriteLock * newKey) throw (bad_move_write_lock)
+		void move_write_lock(_WriteLock * oldKey, _WriteLock * newKey) throw (helper::bad_move_write_lock)
 		{
 			std::lock_guard<std::mutex> lock(_mutex);
 			if(!_write_lock || _write_lock != oldKey || !newKey)
-				throw bad_move_write_lock();
+				throw helper::bad_move_write_lock();
 			else
 				_write_lock = newKey;
 		}
 
-		void unlock_write(_WriteLock * key) throw (bad_write_unlock)
+		void unlock_write(_WriteLock * key) throw (helper::bad_write_unlock)
 		{
 			std::lock_guard<std::mutex> lock(_mutex);
 
 			if(!_write_lock || _write_lock != key)
-				throw bad_write_unlock();
+				throw helper::bad_write_unlock();
 
 			_write_lock = nullptr;
 		}
@@ -275,38 +271,14 @@ namespace lock
 			}
 		}
 
-		void unlock_read() throw (bad_read_unlock)
-		{
-			for(;;)
-			{
-				std::lock_guard<std::mutex> lock(_mutex);
-
-				if(!_write_lock)
-					if(!_read_lock)
-						throw bad_read_unlock();
-					else
-					{
-						_read_lock--;
-						return;
-					}
-			}
-		}
-
-		T &write(WriteLock<T> * key)
+		void unlock_read() throw (helper::bad_read_unlock)
 		{
 			std::lock_guard<std::mutex> lock(_mutex);
-			// needs write lock and the right key to write.
-			if(!_read_lock && (_write_lock? _write_lock == key : true))
-				return obj;
-			else
-				throw bad_write();
-		}
 
-		const T &read()
-		{
-			std::lock_guard<std::mutex> lock(_mutex);
-			if(!_write_lock)
-				return obj;
+			if(!_read_lock || _write_lock)
+				throw helper::bad_read_unlock();
+			
+			_read_lock--;
 		}
 	};
 
@@ -323,7 +295,7 @@ namespace lock
 	public:
 		ReadLock() : proxy(nullptr), object(nullptr) { }
 		/*Blocks the current thread until a lock on the resource could be optained.*/
-		ReadLock(ThreadSafe<T> &proxy) : proxy(&proxy), object(nullptr) { proxy.lock_read(); object = reinterpret_cast<const T*>(&reinterpret_cast<const char &>(proxy.read())); }
+		ReadLock(ThreadSafe<T> &proxy) : proxy(&proxy), object(nullptr) { proxy.lock_read(); object = reinterpret_cast<const T*>(&reinterpret_cast<const char &>(proxy.obj)); }
 		ReadLock(const _MyT &other) : proxy(other.proxy), object(other.object) { if(proxy) proxy->lock_read(); }
 		ReadLock(_MyT &&move) : proxy(move.proxy), object(move.object)
 		{
@@ -372,7 +344,7 @@ namespace lock
 	public:
 		WriteLock() : proxy(nullptr), object(nullptr) { }
 		/*Blocks the current thread until a lock on the resource could be optained.*/
-		WriteLock(ThreadSafe<T> &proxy) : proxy(&proxy), object(nullptr) { proxy.lock_write(this); object = reinterpret_cast<T*>(&reinterpret_cast<T&>(proxy.write(this))); }
+		WriteLock(ThreadSafe<T> &proxy) : proxy(&proxy), object(nullptr) { proxy.lock_write(this); object = reinterpret_cast<T*>(&reinterpret_cast<T&>(proxy.obj)); }
 		WriteLock(const _MyT &) = delete;
 		WriteLock(_MyT && move) : proxy(move.proxy), object(move.object)
 		{
